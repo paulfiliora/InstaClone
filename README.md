7730eb8b850765a4188b4331c30cc5cdbe177c02
Paul Filiora <paul.filiora@gmail.com>
2017-04-13 18:09:38 -0400

    added readme

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-# InstaClone
+# Instagram Clone Assignmnet - Group Project
 
 https://medium.com/@the_taqquikarim/implement-an-instagram-clone-6bebeb51b8a1
+
+Features
+Supports 4 core features: Authentication, Post Creation, User Following, and Activity Feed Generation.
+1) Authenticating Users — allows for a user to register for, log into, and deactivate an account.
+2) Creating Posts — includes uploading photos with captions, and viewing previously uploaded photos on a profile page.
+3) Supporting Followers — users can navigate to another user profile page and choose to follow them and their updates.
+4) Supporting Feeds — on log in, a feed is generated that contains all the activity performed by users that you have chosen to follow. So if your app contains 4 users: A, B, C, and D — if you choose to follow users A and C your feed page should display only updates from users A and C but not updates from users B and D.
+
+MILESTONE 1: Initial setup
+    Assign feature-set owners.
+    Create a main repo and have team members fork from it.
+    Set up initial documentation structure. Get started on writing docs.
+    Plan out the main project including schema, FE framework, API routes, etc.
+    Create initial branches indicating the start of work on specific features.
--- migrations/001-initial-schema.sql
+++ migrations/001-initial-schema.sql
@@ -1,39 +1,22 @@
 -- UP
-CREATE TABLE employee(
+CREATE TABLE User(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL
 );
-CREATE table dept(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL
+CREATE table Follower(
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    follower_id TEXT NOT NULL
 );
-CREATE table employee_dept(
+CREATE table Activity(
     user_id INTEGER NOT NULL,
-    dept_id INTEGER NOT NULL
+    activity_type_id TEXT NOT NULL,
+    activity_payload TEXT NOT NULL
 );
 
-INSERT into employee (name) VALUES ('taq');
-INSERT into employee (name) VALUES ('baba');
-INSERT into employee (name) VALUES ('fizal');
-INSERT into employee (name) VALUES ('liza');
-INSERT into employee (name) VALUES ('nicole');
-
-INSERT into dept (name) VALUES ('consultant');
-INSERT into dept (name) VALUES ('staff');
-
-INSERT into employee_dept (user_id, dept_id) VALUES (1, 1);
-
-INSERT into employee_dept (user_id, dept_id) VALUES (2, 1);
-
-INSERT into employee_dept (user_id, dept_id) VALUES (3, 1);
-INSERT into employee_dept (user_id, dept_id) VALUES (3, 2); 
-
-INSERT into employee_dept (user_id, dept_id) VALUES (4, 2);
-
-INSERT into employee_dept (user_id, dept_id) VALUES (5, 1);
+-- INSERT into user (name) VALUES ('defaultUser');
 
 -- DOWN
 
-DROP TABLE employee;
-DROP TABLE dept;
-DROP TABLE employee_dept;
+DROP TABLE User;
+DROP TABLE Follower;
+DROP TABLE Activity;
