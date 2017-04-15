65215a9510c954f0b854a094116ff35083c4f32f
Dzhama <j.shoismatuloev@gmail.com>
2017-04-15 14:33:26 -0400

    UI sample is ready. Add post page will be add later.r

---
+++ public/js/main.js
@@ -0,0 +1,100 @@
+(function() {// protect the lemmings
+	function GET(url) {
+		return new Promise((resolve, reject) => {
+			const request = new XMLHttpRequest();
+			request.open('GET', url);
+			request.onload = () => {
+				const data = JSON.parse(request.responseText);
+				resolve(data)
+			}; 
+			request.onerror = (err) => {
+				reject(err)
+			};
+			request.send();
+		});
+	} // GET
+
+	
+	function POST(url, data) {
+		return new Promise((resolve, reject) => {
+			const request = new XMLHttpRequest();
+			request.open('POST', url);
+			request.setRequestHeader('Content-Type', 'application/json');
+
+			request.onload = () => {
+				const data = JSON.parse(request.responseText);
+				resolve(data)
+			}; 
+			request.onerror = (err) => {
+				reject(err)
+			};
+
+			request.send(JSON.stringify(data));
+		});
+	} // POST
+
+	function PUT(url, data) {
+		return new Promise((resolve, reject) => {
+			const request = new XMLHttpRequest();
+			request.open('PUT', url);
+			request.setRequestHeader('Content-Type', 'application/json');
+
+			request.onload = () => {
+				const data = JSON.parse(request.responseText);
+				resolve(data)
+			}; 
+			request.onerror = (err) => {
+				reject(err)
+			};
+
+			request.send(JSON.stringify(data));
+		});
+	} // POST
+
+	function DELETE(url, data) {
+	return new Promise((resolve, reject) => {
+		const request = new XMLHttpRequest();
+		request.open('DELETE', url);
+		request.setRequestHeader('Content-Type', 'application/json');
+
+		request.onload = () => {
+			const data = JSON.parse(request.responseText);
+			resolve(data)
+		}; 
+		request.onerror = (err) => {
+			reject(err)
+		};
+
+		request.send(JSON.stringify(data));
+	});
+} // DELETE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
