a335170bb77dce14431354b2adc16a6032642ff0
Paul Filiora <paul.filiora@gmail.com>
2017-04-17 15:27:57 -0400

    GET requests functioning properly in the file focused approach

--- apiroutes.js
+++ apiroutes.js
@@ -1,61 +1,172 @@
 const express = require('express');
 
+let app = express();
+
 const router = express.Router();
 
-const blogApp = require('./blogApp')
+const instaApp = require('./instaApp')
 
-// body parser middleware
 const parser = require('body-parser');
+router.use(parser.json())
 
-//parses requests with the content type of `application/json`
-router.use(parser.json());
+const db = require('sqlite');
+const DB_NAME = './database.sqlite';
 
-//get posts
-router.get('/posts', (request, response, next) => {
-	next();
+
+const socket = require('./sqliteui/websocket');
+router.use('/', express.static('./sqliteui/public', {
+	'index': ['index.html']
+}));
+
+
+const SocketInst = socket(DB_NAME, express);
+app = SocketInst.app;
+
+// router.use((req, res, next) => {
+// 	console.log("in the use")
+// 	let args = {};
+// 	for (const prop in req.body) {
+// 		// console.log(prop, req.body[prop]);
+// 		args['$' + prop] = req.body[prop];
+// 	}
+// 	req.body = args;
+// 	next();
+// })
+
+// ## Get all users + their activity
+router.get('/users', (req, res, next) => {
+	instaApp.getUsers(req, res)
+        .then((data) => {
+            res.header('Content-Type', 'application/json');
+            res.send({ users: data });
+        })
+        .catch((e) => {
+            res.status(401);
+        });
 });
 
-//get post based on ID
-router.get('/post/:postId', (request, response, next) => {
-	const id = parseInt(request.params.postId, 10);
-	blogApp.getItem(id);
-
-	response.header('Content-Type', 'application/json');
-	response.send(blogApp.getItem(id));
+// Get a specified user via user.id + their activity
+router.get('/user/:user_id', (req, res, next) => {
+	const id = parseInt(req.params.user_id, 10);
+	console.log(id)
+	instaApp.getUser(id)
+	    .then((data) => {
+	        // SocketInst.broadcast('LOAD_BUFFER');
+	        res.header('Content-Type', 'application/json');
+	        res.send({
+	            user: data,
+	            numResults: data.length
+		        });
+		    })
+	    .catch((e) => {
+	        console.log(e)
+	        res.status(401);
+    });
 });
 
-
-// post posts
-router.post('/posts', (request, response, next) => {
-	const requestBody = request.body;
-
-	// Add a post
-	blogApp.createItem(requestBody);
-
-	next();
+// get users that $user_id follows
+router.get('/:follower_id/followedusers', (req, res) => {
+	console.log("hey")
+	const id = parseInt(req.params.follower_id, 10);
+	instaApp.getFollowed(id)
+    .then((data) => {
+        // SocketInst.broadcast('LOAD_BUFFER');
+        res.header('Content-Type', 'application/json');
+        res.send({
+            followed_users: data,
+            numResults: data.length
+        });
+    })
+    .catch((e) => {
+        console.log(e)
+        res.status(401);
+    });
 });
 
-// put post
-router.put('/post/:id', (request, response, next) => {
-	const id = parseInt(request.params.id, 10);
-	const dataPayload = request.body;
-	Object.keys(dataPayload).forEach((key) => {
-		blogApp.updateItem(id, 'data.' + key, dataPayload[key]);
-	})
-	next();
-}); // post
+// create a post
+// router.post('/:user_id/post', (req, res, next) => {
+// 	// const requestBody = req.body;
+// 	instaApp.createPost(req)
+//  	// .then((user) => {
+//   //           // SocketInst.broadcast('LOAD_BUFFER');
+//   //           return db.get('SELECT * FROM activities WHERE activities.id = ?', [activities.lastID])
+//   //       })
+//         .then((data) => {
+//         	console.log("at step 5 route.js")
+//             res.header('Content-Type', 'application/json');
+//             res.send({
+//                 activities: data
+//             });
+//         })
+//         .catch((e) => {
+//             res.status(401);
+//         });
+// });
 
-// delete post
-router.delete('/post/:id', (request, response, next) => {
-	const id = parseInt(request.params.id, 10);
-	blogApp.deleteItem(id);
-	next();
-}); // delete
+// router.post('/:user_id/post', (req, res, next) => {
+// 	    let args = {};
+//      for (const prop in req.body) {
+//          args['$' + prop] = req.body[prop];
+//      }
+//      req.body = args;
+//     db.all('SELECT * FROM activities')
+//         .then(() => {
+//             console.log(req.body)
+//             db.run("INSERT INTO activities (user_id, image_url, descr) values ($user_id, $image_url, $descr)", req.body)
+//         })
+//         .then((user) => {
+//         	console.log("after insert")
+//             // SocketInst.broadcast('LOAD_BUFFER');
+//             // db.get('SELECT * FROM activities WHERE activities.id = ?', [activities.lastID])
+//         })
+//         .then((data) => {
+//         	console.log("after where")
+//             res.header('Content-Type', 'application/json');
+//             res.send({ activities: data });
+//         })
+//         .catch((e) => {
+//             res.status(401);
+//         });
+// });
 
-router.use((request, response) => {
-	response.header('Content-Type', 'application/json');
-	response.send(blogApp.getItems());
-});
+// router.follow('/:user_id/follow', (req, res, next) => {
+// 	// const requestBody = req.body;
+// 	instaApp.follow(req)
+//  	// .then((user) => {
+//   //           // SocketInst.broadcast('LOAD_BUFFER');
+//   //           return db.get('SELECT * FROM activities WHERE activities.id = ?', [activities.lastID])
+//   //       })
+//         .then((data) => {
+//         	console.log("at step 5 route.js")
+//             res.header('Content-Type', 'application/json');
+//             res.send({
+//                 activities: data
+//             });
+//         })
+//         .catch((e) => {
+//             res.status(401);
+//         });
+// });
 
+
+// router.use((req, res) => {
+// 		res.header('Content-Type', 'application/json');
+//         res.send({ results: data });
+// 	.catch((e) => {
+// 		console.log(e)
+// 		res.status(401);
+// 	});
+// });
+
+Promise.resolve()
+	.then(() => db.open(DB_NAME, {
+		Promise
+	}))
+// 	// .then(() => db.migrate({ force: 'last' }))
+// 	.then(() => app.listen(port))
+// 	.then(() => {
+// 		console.log(`Server started on port ${port}`)
+// 	})
+// 	.catch(err => console.error(err.stack))
 
 module.exports = router;
---
+++ instaApp.js
@@ -0,0 +1,96 @@
+    const db = require('sqlite');
+const DB_NAME = './database.sqlite';
+
+// const socket = require('./sqliteui/websocket');
+// app.use('/', express.static('./sqliteui/public', {
+//     'index': ['index.html']
+// }));
+
+// const SocketInst = socket(DB_NAME, app);
+// app = SocketInst.app;
+
+const instaApp = {};
+
+// ##Get all users + their activity
+instaApp.getUsers = () => {
+    return db.all(`SELECT * FROM users 
+                    INNER JOIN activities ON activities.user_id = users.id`)
+};
+
+// ##Get a specified user via user.id + their activity
+// ##when getting self, needs to be adapted to grab user_id via session
+instaApp.getUser = (user_id) => {
+    return db.all(`SELECT * FROM users 
+            INNER JOIN activities ON activities.user_id = users.id
+            WHERE users.id = ${user_id}`)
+};
+
+//Get users that $user_id follows
+instaApp.getFollowed = (follower_id) => {
+    return db.all(`SELECT 
+                users.first_name AS user_fname,
+                users.last_name AS user_lname,
+                activities.image_url AS image,
+                activities.descr AS description
+            FROM users
+                INNER JOIN followers ON followers.user_id = users.id 
+                INNER JOIN activities ON activities.user_id = users.id
+            WHERE followers.follower_id = ${follower_id}`)
+};
+
+//Create a post 
+//Needs to be adapted to grab user_id via session
+instaApp.createPost = (req) => {
+    let args = {};
+     for (const prop in req.body) {
+         args['$' + prop] = req.body[prop];
+     }
+     req.body = args;
+     console.log(args)
+    return db.run("INSERT INTO activities (user_id, image_url, descr) values ($user_id, $image_url, $descr)", req.body)
+    console.log("did the insert")
+             .then((user) => {
+
+//             SocketInst.broadcast('LOAD_BUFFER');
+            return db.get('SELECT * FROM activities WHERE activities.id = ?', [activities.lastID])
+        })
+
+        // .then(() => {
+        //     console.log("at step 2 app.js")
+        //     console.log(user_id)
+        //     return db.run("INSERT INTO activities (user_id, image_url, descr) values ($user_id, $image_url, $descr)", req.body)
+        //     console.log("at step 2.5 app.js")
+        // })
+        // .then((user) => {
+        //     console.log("at step 3 app.js")
+        //     // SocketInst.broadcast('LOAD_BUFFER');
+        //     return db.get('SELECT * FROM activities WHERE activities.id = ?', [activities.lastID])
+        //      console.log("at step 4 app.js")
+        // })
+        // .then((data) => {
+        //     res.header('Content-Type', 'application/json');
+        //     res.send({
+        //         activities: data
+        //     });
+        // })
+        // .catch((e) => {
+        //     res.status(401);
+        // });
+};
+
+instaApp.followUser = () => {
+    console.log("hey how ya doing")
+};
+
+// Promise.resolve()
+//     .then(() => db.open(DB_NAME, {
+//         Promise
+//     }))
+//     // .then(() => db.migrate({ force: 'last' }))
+//     .then(() => app.listen(port))
+//     .then(() => {
+//         console.log(`Server started on port ${port}`)
+//     })
+//     .catch(err => console.error(err.stack))
+
+module.exports = instaApp
