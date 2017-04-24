860b2f5fbdb92d53c75000ace226071775dc0c6c
Paul Filiora <paul.filiora@gmail.com>
2017-04-23 23:32:26 -0400

    prepped login with session storage, registration, and firebase

--- passport.js
+++ passport.js
@@ -1,24 +1,6 @@
-module.exports = function (app, db) {
+module.exports = function(app, db) {
     const passport = require('passport');
     const LocalStrategy = require('passport-local').Strategy;
-
-
-
-    passport.use(new LocalStrategy({
-        usernameField: 'email',
-        passwordField: 'password',
-    }, (email, password, done) => {
-        console.log(email, password)
-        if (!email || !password) {
-            return done('error', {}, {});
-        }
-        db.get(`SELECT users.first_name, users.email, users.id FROM users WHERE users.email = '${email}' AND users.password = '${password}'`)
-            .then((row) => {
-                console.log(row)
-                if (!row || row.length === 0) return done(true, false);
-                return done(null, row);
-            })
-    }));
 
     passport.serializeUser((user, done) => {
         console.log('serializing user', user)
@@ -26,57 +8,75 @@ module.exports = function (app, db) {
     });
 
     passport.deserializeUser((user, done) => {
-        console.log('deserializing user', user)
         done(null, user)
     });
 
-    app.use(passport.initialize());
-    app.use(passport.session());
-
-    app.post('/auth/login', (req, res, next) => {
-
-        passport.authenticate('local', function (err, user, info) {
-            passport.authenticate('local', (err, user, info) => {
-                // console.log('about to authenticate')
-                // console.log('err, user, info ---- ', err, user, info)
-                if (err || !user) {
-                    console.log('err is :', err);
-                    next()
-                }
-
-                req.logIn(user, (err) => {
-                    console.log('now in req login', err)
-                    if (err) return next(err);
-                    // res.header('Content-Type', 'application/json');
-                    // res.send({
-                    //     success: true,
-                    //     id: user.id
-                    // });
-                    next()
-                });
-            })(req, res, next);
-        });
-    })
-
-    app.use((req, res, next) => {
-        console.log('in middleware')
-        console.log('req.user :', req.user)
-        console.log('req.session :', req.session)
-        console.log('req.isAuth :', req.isAuthenticated());
-        const user = req.user
-
-        if (req.isAuthenticated()) {
-            console.log('authenticated')
-            res.redirect('/home');
-            // next();
-            return;
+    //at least checks the db for combo, returns username & id
+    passport.use(new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+    }, (email, password, done) => {
+        // console.log(email, password)
+        if (!email || !password) {
+            return done('error', {}, {});
         }
-        res.redirect('/')
-        // res.header('Content-Type', 'application/json');
-        // res.status(403);
-        // res.send({
-        //     success: false,
-        // })
+
+        db.get(`SELECT users.first_name, users.email, users.id FROM users WHERE users.email = '${email}' AND users.password = '${password}'`)
+            .then((row) => {
+                console.log('row is :',row)
+                if (!row || row.length === 0) return done(true, false);
+                return done(null, row);
+            })
+    }));
+    app.use(passport.initialize());
+    app.use(passport.session());  
+
+    app.get('/auth/logout', (request, response, next) => {
+        request.logout();
+        next();
+        // response.redirect('/');
+    });
+
+    app.post('/auth/login', (request, response, next) => {
+        passport.authenticate('local', (err, user, info) => {
+            // console.log('about to authenticate')
+            console.log('err, user, info ---- ', err, user, info)
+            if (err || !user) {
+                console.log('err is :',err);
+               next()
+            }
+            
+            request.logIn(user, (err) => {
+                // console.log('now in req login', err)
+                if (err) return next(err);
+                response.header('Content-Type', 'application/json');
+                response.send({
+                    success: true,
+                    id: user.id
+                })
+                next()
+
+            });
+        })(request, response, next);
+    });
+
+    app.use((request, response, next) => {
+        // console.log('in middleware')
+        console.log('req.user :',request.user)
+        // console.log('req.session :',request.session)
+        console.log('req,isAuth :',request.isAuthenticated());
+
+        if (request.isAuthenticated()) {
+            console.log('shit is authenticated')
+            next();
+            return ;
+        }
+
+        response.header('Content-Type', 'application/json');
+        response.status(403);
+        response.send({
+            success: false,
+        })
     })
 
     return passport;
